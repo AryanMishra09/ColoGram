ff75fc91b6ab1629d3875b852bf4eae92673ec79
Aryan Mishra <mishraaryan9204@gmail.com>
2024-01-21 14:42:34 +0530

    connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log('MongoDB is connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 
 app.listen(3000, () => {
