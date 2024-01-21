8c57e930096aadc762f089d99cfabd1dab8fd70f
Aryan Mishra <mishraaryan9204@gmail.com>
2024-01-21 12:55:55 +0530

    created header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,13 @@ import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+
+      <Header/>
 
       <Routes>
 
