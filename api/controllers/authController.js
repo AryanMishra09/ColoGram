845fbe41ae7eaa5bf42a8033c5250c6a3fc1d5c0
Aryan Mishra <mishraaryan9204@gmail.com>
2024-01-22 10:07:35 +0530

    Created singin api route (backend)

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,8 +1,9 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-
+// for signing up a user 
 export const signup = async(req, res, next) => {
     const {username, email, password } = req.body;
     if(!username || !email || !password || username === '' || email === '' || password === ''){
@@ -24,5 +25,35 @@ export const signup = async(req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
+
+// for sigining in a user: 
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, "All fields are required!"));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if(!validUser){
+           return  next(errorHandler(404, "User not Found!"));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400, "Invalid password"));
+        }
+
+        const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET_KEY );
+
+        const {password: pass, ...rest} = validUser._doc;
+        res.status(200)
+            .cookie('access_token', token, { httpOnly: true})
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
+
 };
 
--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/authController.js';
+import { signin, signup } from '../controllers/authController.js';
 
 const router = express.Router();
 
 // for signup a user : (/api/auth/signup):
 router.post('/signup', signup);
+
+// for signin a user : (/api/auth/signin):
+router.post('/signin', signin);
 
 export default router;
