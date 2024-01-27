af9eb8174dd668539e5806b0d3902c3b35e59dcb
Aryan Mishra <mishraaryan9204@gmail.com>
2024-01-27 16:58:53 +0530

    added delete user api route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,7 +3,7 @@ import { errorHandler } from '../utils/error.js';
 import User from '../models/userModel.js';
 
 
-
+//update user (/api/user/update/:userId)
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.userId) {
       return next(errorHandler(403, 'You are not allowed to update this user'));
@@ -50,4 +50,17 @@ export const updateUser = async (req, res, next) => {
       } catch (error) {
         next(error);
       }
+};
+
+//delete user (/api/user/delete/:userId):
+export const deleteUser = async(req, res, next) => {
+  if(req.user.id !== req.params.userId){
+    return next(errorHandler(403, "You are not allowed to delete this user "));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
 };
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { updateUser } from '../controllers/userController.js';
+import { deleteUser, updateUser } from '../controllers/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 
 export default router;
