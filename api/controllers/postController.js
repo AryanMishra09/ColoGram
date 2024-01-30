b1c9deb929114dab5f07f79295c36ddf5c7f8c83
Aryan Mishra <mishraaryan9204@gmail.com>
2024-01-30 18:56:10 +0530

    Added delete post functionality to the dashboard

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -60,3 +60,16 @@ export const getposts = async (req, res, next) => {
     next(error);
   }
 };
+
+//to delete post (/api/post/deleteposts/:postId/:userId):
+export const deletepost = async (req, res, next) => {
+  if(!req.user.isAdmin || req.user.id !== req.params.userId){
+    return next(errorHandler(403, "You are not allowed to delete this post!"));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json("The post has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -270,7 +270,7 @@ export default function DashProfile() {
                 <div className="text-center">
                   <HiOutlineExclamationCircle className="h-14 w-14 text-gray-800 light:text-gray-200 mb-4 mx-auto" />
                   <h3 className="mb-5 text-lg text-gray-800 light:text-gray-200">
-                    Are you sure you want to delete ypur account? 
+                    Are you sure you want to delete your account? 
                   </h3>
                   <div className="flex justify-center gap-8">
                     <Button color="failure"  onClick={handleDeleteUser}>
