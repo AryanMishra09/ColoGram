5801190ce02ff1b9fa03f98a242d176bdef29051
Aryan Mishra <mishraaryan9204@gmail.com>
2024-02-02 13:10:32 +0530

    Completed post page functionlaity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 
 export default function App() {
   return (
@@ -44,6 +45,8 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp/>} />
 
         <Route path='/projects' element={<Projects/>} />
+        
+        <Route path='/post/:postSlug' element={<PostPage />} />
 
       </Routes>
 
