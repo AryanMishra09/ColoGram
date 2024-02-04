6f148cdcafab839017fc245ef9fde277efa0062b
Aryan Mishra <mishraaryan9204@gmail.com>
2024-02-04 19:03:14 +0530

    Added edit functionality to the comment section

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 
 import { Alert, Button, Textarea } from 'flowbite-react';
 import { useEffect, useState } from 'react';
@@ -81,7 +82,15 @@ export default function CommentSection({postId}) {
         } catch (error) {
           console.log(error.message);
         }
-      };
+    };
+
+    const handleEdit = async (comment, editedContent) => {
+        setComments(
+          comments.map((c) =>
+            c._id === comment._id ? { ...c, content: editedContent } : c
+          )
+        );
+    };
 
     return (
         <div className='max-w-2xl mx-auto w-full p-3'>
@@ -145,7 +154,7 @@ export default function CommentSection({postId}) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
           ))}
         </>
       )}
