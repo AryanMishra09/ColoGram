157ec067cfe5848868408d50f8c1d2df976e7d7b
Aryan Mishra <mishraaryan9204@gmail.com>
2024-02-04 18:21:54 +0530

    Added like functionality to the comments

--- client/src/components/Coment.jsx
+++ client/src/components/Coment.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
 import moment from 'moment';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { FaThumbsUp } from 'react-icons/fa';
 
-export default function Comment({ comment }) {
+export default function Comment({ comment, onLike }) {
   const [user, setUser] = useState({});
-//   console.log(user);
+  const { currentUser } = useSelector((state) => state.user);
+  //console.log(user);
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -36,6 +39,25 @@ export default function Comment({ comment }) {
           <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
         </div>
         <p className='text-gray-500 pb-2'>{comment.content}</p>
+        <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+          <button
+            type='button'
+            onClick={() => onLike(comment._id)}
+            className={`text-gray-400 hover:text-blue-500 ${
+              currentUser &&
+              comment.likes.includes(currentUser._id) &&
+              '!text-blue-500'
+            }`}
+          >
+            <FaThumbsUp className='text-sm' />
+          </button>
+          <p className='text-gray-400'>
+            {comment.numberOfLikes > 0 &&
+              comment.numberOfLikes +
+                ' ' +
+                (comment.numberOfLikes === 1 ? 'like' : 'likes')}
+          </p>
+        </div>
       </div>
     </div>
   );
