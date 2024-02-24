bd2f2f01e65f7a724d454badeb2da6ce7b7a1670
Aryan Mishra <mishraaryan9204@gmail.com>
2024-02-25 00:30:58 +0530

    Changes for deployment

--- api/index.js
+++ api/index.js
@@ -31,9 +31,10 @@ app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res)=>{
-    res.sendFile((path.join(__dirname, 'client', 'dist', 'index.html')))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
