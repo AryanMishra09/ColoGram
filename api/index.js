2389ac55c877a07bc62e4b7e6f1ea388e4097bb0
Aryan Mishra <mishraaryan9204@gmail.com>
2024-02-25 00:17:12 +0530

    Added pre development changes for Render deploy

--- api/index.js
+++ api/index.js
@@ -6,11 +6,14 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoute.js';
 import postRoutes from './routes/postRoutes.js';
 import commentRoutes from './routes/commentRoutes.js';
+import path from "path";
 
 dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+const __dirname = path.resolve();
 
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
@@ -26,6 +29,11 @@ app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res)=>{
+    res.sendFile((path.join(__dirname, 'client', 'dist', 'index.html')))
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
